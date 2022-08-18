b791558c153f9e3286b7b823059c8207fbf9841c
Arokumi <aam71788@gmail.com>
2022-08-18 08:04:36 +0700

    Mac Update - 18 Aug

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
-function submitData() {
-    let name = document.getElementById('username').value;
-    document.getElementsByClassName('username').innerHTML = name;
-};
+// function submitData() {
+//     let name = document.getElementById('username').value;
+//     document.getElementsByClassName('username').innerHTML = name;
+// };
 function submitFn(){
     event.preventDefault()
-    location.href = "http://127.0.0.1:5500/portofolio.html"
+    let name = document.getElementById('username').value;
+    document.getElementById('Pname').innerHTML = name;
 };
+
+
+// function primeChecker (number) {
+//         let isPrime = true;
+//         if (number === 1) {
+//             console.log('1 is neither a prime nor composite number')
+//             isPrime = undefined;
+//         }
+//         else if (number > 1) {
+//             for (let i = 2; i < number; i++) {
+//                 if (number % i == 0) {
+//                     isPrime = false;
+//                     break;
+//                 }
+//             }
+//         }
+        
+//         console.log(isPrime);
+//     }
+    
+//     primeChecker (103);
